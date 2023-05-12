53e71965c43c020627c73e1ee34e564d21b04264
oluwaleke21 <oluwaleke21@gmail.com>
2023-05-12 07:57:34 +0100

    Leke Biography

---
+++ script.js
@@ -0,0 +1,6 @@
+// create a js function to change "I am currently learning HTML and CSS" to "I am learning JavaScript"
+// when the "Update Me"button is clicked
+
+function changeText() {
+    document.getElementById("my-text").innerHTML = "I    AM    CURRENTLY    LEARNING    JAVASCRIPT"
+}
